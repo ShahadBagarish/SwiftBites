1cfaa59af576c2ec8f2ad8581220930584ffe946
Shahad Bagarish <shahad.bagarish@gmail.com>
2024-10-09 14:50:07 +0300

    Setup SwiftData Basics

---
+++ SwiftBites/DefaultData.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultData.swift
+//  SwiftBites
+//
+//  Created by Shahad Bagarish on 09/10/2024.
+//
+
+import Foundation
---
+++ SwiftBites/ModelContainer.swift
@@ -0,0 +1,8 @@
+//
+//  ModelContainer.swift
+//  SwiftBites
+//
+//  Created by Shahad Bagarish on 09/10/2024.
+//
+
+import Foundation
